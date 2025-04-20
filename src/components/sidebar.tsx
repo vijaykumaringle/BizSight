33d2d024eac8cf4dbadf9dbe32878e93f9a11c0b
Vijaykumar <vijaykumar.ingle01@gmail.com>
2025-04-20 16:40:36 +0000

    few fixes 20 April 3.0

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react"
+import React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
 import { PanelLeft } from "lucide-react"
@@ -18,6 +18,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+
+import { SidebarMenu } from "./sidebar";
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
@@ -761,6 +763,7 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
 
 import { Icons } from "./icons"
 
